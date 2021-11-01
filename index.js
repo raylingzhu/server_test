6bff3c70d58d6af9dfe207779db66cf01fb47081
ZHU RAY <raylingzhu@hotmail.com>
2021-11-01 11:28:37 +1100

    make it better

---
+++ index.js
@@ -0,0 +1,15 @@
+const WebSocket = require("ws");
+
+const por = process.env.PORT || 8082 
+
+const wss = new WebSocket.Server({port:por});
+
+wss.on('connection', ws => {
+    console.log("connection established");
+
+
+
+    ws.on(close, () => {
+        console.log("a client has disconnected");
+    })
+} );
